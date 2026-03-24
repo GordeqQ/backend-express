917afc44176ebbaf9f83ad219c4dde90f9cc72e6
Roman Novikov <novikovroman31072006@mail.ru>
2026-03-24 17:14:57 +0500

    task 5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+let users = [{"id":1, "name":"Roma Novikov"}, {"id":2, "name": "Gordey Aleksandrov"}];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   let items = [{"id":1, "name":"Roma Novikov"}, {"id":2, "name": "Gordey Aleksandrov"}];
   res.set(items);
 });
+
+router.post('/', function(req, res, next){
+  let newUser = {"id":users.length, "name":req.body}
+  users.push(newUser);
+  res.status(201).json(newUser);
+})
 
 module.exports = router;
