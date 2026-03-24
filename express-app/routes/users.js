b8f384b7eda144358580523adba2b79f649271a5
GordeqQ <defjalox@gmail.com>
2026-03-24 17:23:22 +0500

    task 5 refactor

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,12 +3,12 @@ const router = express.Router();
 let users = [{"id":1, "name":"Roma Novikov"}, {"id":2, "name": "Gordey Aleksandrov"}];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let items = [{"id":1, "name":"Roma Novikov"}, {"id":2, "name": "Gordey Aleksandrov"}];
-  res.set(items);
+  let items = users;
+  res.json(items);
 });
 
 router.post('/', function(req, res, next){
-  let newUser = {"id":users.length, "name":req.body}
+  let newUser = {"id":users.length + 1, "name":req.body.name}
   users.push(newUser);
   res.status(201).json(newUser);
 })
